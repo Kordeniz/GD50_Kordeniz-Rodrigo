7a3cce858811b92d40846f003b6995f8c2262f11
Kordeniz <kordeniz.tanil@gmail.com>
2018-02-19 21:52:00 -0800

    Add files via upload

---
+++ GD50_Kordeniz-Rodrigo-master/ConsoleApp4/ConsoleApp4/MonsterTemplate.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class MonsterTemplate
+    {
+        protected string sMonsterName; // Name of the monster
+        public int iMonsterHealth; // Health of the monster
+        protected int iMinimumDamage; // Minimum damage of player
+        protected int iMaximumDamage; // Maximum damage of player
+        protected int iMonsterDodgeValue; // Dodge value of player
+        protected int iMonsterAccuracy; // Hit chance of player
+        protected bool isDead;
+        Random rRNGesus = new Random();// Rand(iMinimumDamage,iMaximumDamage);
+
+        public int Dodge
+        {
+            get
+            {
+                return iMonsterDodgeValue;
+            }
+
+            set
+            {
+                iMonsterDodgeValue = value;
+            }
+        }
+
+        public int MonsterHealth
+        {
+            get
+            {
+                return iMonsterHealth;
+            }
+
+            set
+            {
+                iMonsterHealth = value;
+                if(iMonsterHealth>0)
+                {
+                    isDead = false;
+                }
+                else
+                {
+                    isDead = true;
+                }
+            }
+        }
+
+        public bool MonsterAttack(int PlayerDodge)
+        {
+            int iTempAttackRoll = 0;
+            iTempAttackRoll = rRNGesus.Next(1, 21);
+            iTempAttackRoll = +iMonsterAccuracy;
+
+            if (iTempAttackRoll >= PlayerDodge)
+            {
+                Console.WriteLine("Bad boi hit you");
+                return true;
+
+            }
+
+            else
+            {
+                Console.WriteLine("Bad boi missed! YEAH BOI");
+                return false;
+
+            }
+
+        }
+        public int MonsterDamage(bool MonsterAttack)
+        {
+
+            if (MonsterAttack)
+            {
+                return rRNGesus.Next(iMinimumDamage, ((iMaximumDamage + 1)));
+
+            }
+
+            else
+            {
+                Console.WriteLine("Missed");
+                return 0;
+            }
+
+        
+        }
+        public int MonsterAttackTemp(int PlayerDodge)
+        {
+            bool isSuccess = MonsterAttack(PlayerDodge);
+            return MonsterDamage(isSuccess);
+        }
+
+    }
+    class Goblin : MonsterTemplate
+    {
+        public Goblin()
+        {
+            sMonsterName = "Gob Gob Boi";
+            iMonsterHealth = 20;
+            iMinimumDamage = 9;
+            iMaximumDamage = 16;
+            iMonsterDodgeValue = 4;
+            iMonsterAccuracy = 7;
+        }
+    }
+    class Elf : MonsterTemplate
+    {
+        public Elf()
+        {
+            sMonsterName = "Pointi Boi";
+            iMonsterHealth = 30;
+            iMinimumDamage = 11;
+            iMaximumDamage = 20;
+            iMonsterDodgeValue = 6;
+            iMonsterAccuracy = 8;
+        }
+    }
+    class Bat : MonsterTemplate
+    {
+        public Bat()
+        {
+            sMonsterName = "Fliyng Mice Boi";
+            iMonsterHealth = 15;
+            iMinimumDamage = 8;
+            iMaximumDamage = 15;
+            iMonsterDodgeValue = 7;
+            iMonsterAccuracy = 9;
+        }
+    }
+}
---
+++ GD50_Kordeniz-Rodrigo-master/ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isCombat = true;
+            int iDungeonLevel = 0;
+            string sPlayerInput;
+            int iPlayerInput;
+            PlayerClassTemp Boi;
+            Goblin Goblin = new Goblin();
+
+            sPlayerInput = Console.ReadLine();
+            iPlayerInput = PlayerNumberCheck(0, 4, sPlayerInput);
+
+            sPlayerInput = Console.ReadLine();
+
+            //Charater Creation
+            switch (iPlayerInput)
+            {
+                case 1:
+                    {
+                        Boi = new Fat_Boi(sPlayerInput);
+                        break;
+                    }
+                case 2:
+                    {
+                        Boi = new Stabby_Boi(sPlayerInput);
+                        break;
+                    }
+                case 3:
+                    {
+                        Boi = new Wise_Boi(sPlayerInput);
+                        break;
+                    }
+                default:
+                    {
+                        Boi = new Wise_Boi(sPlayerInput);
+                        break;
+                    }
+            }
+
+            Console.WriteLine(Goblin.iMonsterHealth);
+            sPlayerInput = Console.ReadLine();
+            iPlayerInput = PlayerNumberCheck(0, 4, sPlayerInput);
+            Goblin.iMonsterHealth -= Boi.AttackTemp(iPlayerInput, Goblin.Dodge);
+
+            Console.WriteLine(Goblin.iMonsterHealth);
+            Console.ReadLine();
+
+
+
+
+
+
+
+        }
+        static int PlayerNumberCheck(int Min, int Max, String Input)
+        {
+            int Type;
+            bool IsSane;
+            do
+            {
+                IsSane = Int32.TryParse(Input, out Type);
+                if (IsSane && Type > Min && Type < Max)
+                {
+                    Console.WriteLine("MIT Education in It's Best");
+
+                }
+
+                else
+                {
+                    Console.WriteLine("What do they Teach you In MIT 'Close Quarters Crowbar Combat'? Please Try Again");
+                    Input = Console.ReadLine();
+                    IsSane = false;
+                }
+
+            } while (!IsSane);
+
+            return Type;
+        }
+
+    }
+}
